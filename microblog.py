91d529503224c5803437a2715d9919ca787d5206
kris-zwolinski-openhouseAI <kzwolinski@openhouse.ai>
2022-01-25 21:21:24 -0500

    2 functions for password hashing

--- microblog.py
+++ microblog.py
@@ -1,5 +1,11 @@
-from app import app
+from app import app, db
+from app.models import User, Post
 from flask import Flask
 
 
 app = Flask(__name__)
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
