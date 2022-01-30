847a2d1cb2de25fd882a66be01ee44c03a14f1a2
kris-zwolinski-openhouseAI <kzwolinski@openhouse.ai>
2022-01-29 22:21:32 -0500

    user register, rearrange files inside app to look better, and fix user login loop.

--- app/models.py
+++ app/models.py
@@ -1,28 +1,24 @@
-from app import db, login
 from datetime import datetime
+from app import db, login
+from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin, LoginManager, login_manager
 
 
-class User(db.Model, UserMixin):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, index=True, unique=True)
-    email = db.Column(db.String, index=True, unique=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
-        return f"<User {self.username}>"
+        return '<User {}>'.format(self.username)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
-        return check_password_hash(self.password, password)
-
-    @property
-    def is_active(self):
-        return True
+        return check_password_hash(self.password_hash, password)
 
 
 @login.user_loader
@@ -32,9 +28,9 @@ def load_user(id):
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String(200))
+    body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
-        return f"<Post {self.body}>"
+        return '<Post {}>'.format(self.body)
